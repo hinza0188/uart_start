8f801e8d24d98209f3805d1ef01c07378f1516f3
Yongki JayZ An <yxa8247@rit.edu>
2017-02-11 15:24:42 -0500

    updated code with help of TA

--- Timer.c
+++ Timer.c
@@ -11,31 +11,30 @@ void Timer_Init() {
 	GPIOA->AFR[0] = GPIO_AFRL_AFRL1;					// Now the Register is set to AFSEL1
 	
 	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN; // Enable Timer 2 Clock
-	TIM2->PSC &= 80;
+	TIM2->PSC &= 80;											// 80Mhz CPU Pre-Scalar
+	
+	TIM2->CCER = TIM_CCER_CC1NE;  // disable Timer 2 output enable register
 	
 	TIM2->CCMR1 = TIM_CCMR1_CC1S_1; // the channel is now input, CCR1 register is now read-only
 	
-	TIM2->CCER = TIM_CCER_CC1NE;  // disable Timer 2 output enable register
 	TIM2->CCER = TIM_CCER_CC1E;		// enable Timer 2 output enable register
-	
-	//TIM2->CCER |= 0; // turn off Timer 2 output enable resgister
-	//TIM2->CCER |= 1;
-	
 }
 
 
 void run_timer() {
 	int runCount = 1000; // hard coded testing round
+	int result = 0;
 	
 	while (runCount >= 0) {
-		TIM2->CR1 |= TIM_CR1_CEN; // start input capturing
-		printf(capture_event);
+		//TIM2->CR1 |= TIM_CR1_CEN; // start input capturing
+		TIM2->CR1 |= 0x1; // start input capturing
+		result = capture_event();
 		runCount--;
 	}
-	TIM2->CR1 |= 0; // clear the CR1
+	TIM2->CR1 |= 0; // clear the CR1 bit to stop timer
 }
 
-int capture_event(){
+int capture_event(void){
 	while (TIM2->SR == TIM_SR_CC1IF);	// captured an event!
 	return TIM2->CCR1;
 }
